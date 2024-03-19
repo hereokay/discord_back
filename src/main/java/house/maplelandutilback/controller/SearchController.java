6d5306dde0bc010c7d7f2020616c0731469df952
Chris <hereokay88@gmail.com>
2024-03-19 22:21:10 +0900

    feat: 블랙리스트 로직변경 - 10분누적 메시지 100개

--- src/main/java/house/maplelandutilback/controller/SearchController.java
+++ src/main/java/house/maplelandutilback/controller/SearchController.java
@@ -1,5 +1,6 @@
 package house.maplelandutilback.controller;
 
+import house.maplelandutilback.domain.BlockRequest;
 import house.maplelandutilback.domain.Message;
 import house.maplelandutilback.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,17 +19,35 @@ public class SearchController {
     public ResponseEntity<?> search(@RequestParam String keyword) {
 
         // 처리된 검색 결과 반환
-        List<Message> messageList = messageService.performSearch(keyword);
-        System.out.println(messageList.get(0).getContent());
+        List<Message> messageList = messageService.performSearch(keyword,2000);
+
         return ResponseEntity.ok(messageList);
     }
 
     @PostMapping("/socket/addMessage")
     public ResponseEntity<?> addMessage(@RequestBody List<Message> messageList) {
-
-        messageService.checkAndSaveAllMessages(messageList);
+        messageService.saveListNonDuplicate(messageList);
 
         return ResponseEntity.ok().body("Messages saved successfully");
     }
+
+    @PostMapping("/block/addBlock")
+    public ResponseEntity<?> addBlockByKeyword() {
+
+        Long cnt = messageService.blockDetectAndDelete();
+        return ResponseEntity.ok().body("삭제된 갯수 : " + cnt);
+    }
+
+
+
+//    @GetMapping("/test")
+//    public ResponseEntity<?> test() {
+//
+//        messageService.test();
+//
+//        return ResponseEntity.ok().body("Messages saved successfully");
+//    }
+
+
 
 }
