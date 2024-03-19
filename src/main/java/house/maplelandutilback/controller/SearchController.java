4ed243c0258be119053398779cdd8396a3d19701
Chris <hereokay88@gmail.com>
2024-03-19 23:20:08 +0900

    feat: addBlock 함수에 로그추가

--- src/main/java/house/maplelandutilback/controller/SearchController.java
+++ src/main/java/house/maplelandutilback/controller/SearchController.java
@@ -47,9 +47,10 @@ public class SearchController {
     }
 
     @PostMapping("/block/addBlock")
-    public ResponseEntity<?> addBlockByKeyword() {
+    public ResponseEntity<?> addBlock() {
 
         Long cnt = messageService.blockDetectAndDelete();
+        logger.info("Received addBlock request cnt : "+ String.valueOf(cnt));
         return ResponseEntity.ok().body("삭제된 갯수 : " + cnt);
     }
 
